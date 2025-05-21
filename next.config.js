1281445410cfd5453530953faf91baf92d05329f
volcanic <72688335+volcanicll@users.noreply.github.com>
2025-05-21 22:01:38 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,6 +11,9 @@ const nextConfig = {
     SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
     NEXT_PUBLIC_ACCESS_CODE: process.env.NEXT_PUBLIC_ACCESS_CODE,
     RAPID_API_KEY: process.env.RAPID_API_KEY,
+    OKX_PASSPHRASE: process.env.OKX_PASSPHRASE,
+    OKX_SECRET_KEY: process.env.OKX_SECRET_KEY,
+    OKX_API_KEY: process.env.OKX_API_KEY,
   },
 };
 
