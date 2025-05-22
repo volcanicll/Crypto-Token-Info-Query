768473b337db003cf8be4362e1ac7f0f60d70cab
volcanicll <1605090219@qq.com>
2025-05-22 10:38:26 +0800

    Optimize ESLint configuration format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,13 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-{
-   rules: {	
- 	 	"@typescript-eslint/no-unused-vars": "warn"， 	
- 
-"@typescript-eslint/no-explicit-any": "warn",
-}
-}
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
 ];
 
 export default eslintConfig;
